855cbca11c0c9e97479d652567565e8dedc2dd86
Matthew Martin <matt92martin@gmail.com>
2025-02-06 07:20:17 -0800

    tabs

--- wezterm.lua
+++ wezterm.lua
@@ -9,9 +9,63 @@ config.colors = colors
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 14
 
-config.enable_tab_bar = true
+config.tab_max_width = 60
+config.tab_bar_at_bottom = true
 
-config.window_decorations = "INTEGRATED_BUTTONS"
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#0b0022'
+    local background = '#1b1032'
+    local foreground = '#808080'
+
+    if tab.is_active then
+      background = '#2b2042'
+      foreground = '#c0c0c0'
+    elseif hover then
+      background = '#3b3052'
+      foreground = '#909090'
+    end
+
+    wezterm.log_info(config)
+    local edge_foreground = background
+
+    local title = tab_title(tab)
+
+    return {
+      { Background = { Color = 'none' } },
+      { Foreground = { Color = edge_foreground } },
+      -- { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      -- { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
+config.window_decorations = "RESIZE"
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
 
 config.keys = {
   {
@@ -33,7 +87,7 @@ config.macos_window_background_blur = 10
 config.window_padding = {
   left = '1cell',
   right = '1cell',
-  top = 0,
+  top = '0.5cell',
   bottom = 0,
 }
 
